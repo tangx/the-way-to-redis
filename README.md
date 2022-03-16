2f561c971d94b413ff79f6dcde2289129fabf03d
tangx <uyinn@live.com>
2022-03-16 18:08:53 +0800

    demo: 04 pcc 悲观锁

--- README.md
+++ README.md
@@ -14,3 +14,4 @@
   + [v2 - 使用 redis 事务，依然存在超卖](./demos/02-promote-demo/v2/README.md)
   + [v3 - 使用 lua 实现原子操作](./demos/02-promote-demo/v3/README.md)
 3. [分布式锁-乐观锁 - watch + 事务](./demos/03-optimistic-concurrency-control/README.md)
+4. [分布式锁-悲观锁 - 大锁 + 小锁]()
---
+++ demos/04-pessimistic-concurrency-control/config.go
@@ -0,0 +1,25 @@
+package main
+
+import (
+	"context"
+
+	"github.com/go-redis/redis/v8"
+)
+
+var rdb *redis.Client
+
+func init() {
+	rdb = redis.NewClient(
+		&redis.Options{
+			Addr:     "localhost:6379",
+			Password: "redis123",
+			DB:       0,
+		},
+	)
+
+	c := context.Background()
+	if err := rdb.Ping(c).Err(); err != nil {
+		panic(err)
+	}
+
+}
